d0a48c5a30b4da015e92391381e56b187265bcc3
Kedves Nándor <kbugesz@gmail.com>
2016-09-29 22:30:44 +0200

    Time check with other structures
    Added binary tree structure for comparing performance. Currently
    checking performance at insertion and search.

--- main.cpp
+++ main.cpp
@@ -3,25 +3,210 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctime>
+#include <queue>
 
 using namespace std;
+
+typedef int Val_TYPE;
+typedef vector<Val_TYPE> Container_TYPE;
+typedef priority_queue<Val_TYPE, Container_TYPE> pri_queue;
+
+class Queue : public pri_queue {
+public:
+	Container_TYPE::iterator begin() {
+		return pri_queue::c.begin();
+	}
+	Container_TYPE::iterator end() {
+		return pri_queue::c.end();
+	}
+}Q;
+
+///////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
+
+struct node
+{
+	int key_value;
+	node *left;
+	node *right;
+};
+
+class btree
+{
+public:
+	btree();
+	~btree() {
+		destroy_tree();
+	}
+
+	void insert(int key);
+	node *search(int key);
+	void destroy_tree();
+
+private:
+	void destroy_tree(node *leaf);
+	void insert(int key, node *leaf);
+	node *search(int key, node *leaf);
+
+	node *root;
+};
+
+btree::btree()
+{
+	root = NULL;
+}
+
+void btree::destroy_tree(node *leaf)
+{
+	if (leaf != NULL)
+	{
+		destroy_tree(leaf->left);
+		destroy_tree(leaf->right);
+		delete leaf;
+	}
+}
+
+void btree::insert(int key, node *leaf)
+{
+	if (key< leaf->key_value)
+	{
+		if (leaf->left != NULL)
+			insert(key, leaf->left);
+		else
+		{
+			leaf->left = new node;
+			leaf->left->key_value = key;
+			leaf->left->left = NULL;    //Sets the left child of the child node to null
+			leaf->left->right = NULL;   //Sets the right child of the child node to null
+		}
+	}
+	else if (key >= leaf->key_value)
+	{
+		if (leaf->right != NULL)
+			insert(key, leaf->right);
+		else
+		{
+			leaf->right = new node;
+			leaf->right->key_value = key;
+			leaf->right->left = NULL;  //Sets the left child of the child node to null
+			leaf->right->right = NULL; //Sets the right child of the child node to null
+		}
+	}
+}
+
+node *btree::search(int key, node *leaf)
+{
+	if (leaf != NULL)
+	{
+		if (key == leaf->key_value)
+			return leaf;
+		if (key<leaf->key_value)
+			return search(key, leaf->left);
+		else
+			return search(key, leaf->right);
+	}
+	else return NULL;
+}
+
+void btree::insert(int key)
+{
+	if (root != NULL)
+		insert(key, root);
+	else
+	{
+		root = new node;
+		root->key_value = key;
+		root->left = NULL;
+		root->right = NULL;
+	}
+}
+
+node *btree::search(int key)
+{
+	return search(key, root);
+}
+
+void btree::destroy_tree()
+{
+	destroy_tree(root);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
 
 int main()
 {
 
 	Skiplist tmp;
+	btree tree;
 
-	srand(time(NULL));
+	srand(12);
 
-	for (int i = 0; i < 15; i++) {
-		tmp.insert(rand() % 10);
+	clock_t begin;
+
+	int SIZE = 1000000;
+
+	begin = clock();
+	for (int i = 0; i < SIZE; i++) {
+		tmp.insert(rand());
 	}
+	
+	//tmp.print();
+	//cout << endl << endl;
+
+	cout << "Running time in seconds: \t" << double(clock() - begin) / CLOCKS_PER_SEC << endl;
+
+	
+
+	begin = clock();
+	for (int i = 0; i < SIZE; i++) {
+		tmp.search(rand());
+	}
+	cout << "Running time in seconds: \t" << double(clock() - begin) / CLOCKS_PER_SEC << endl;
+	
+	cout << endl;
+
+	begin = clock();
+	for (int i = 0; i < SIZE; i++) {
+		tree.insert(rand());
+	}
+	cout << "Running time in seconds: \t" << double(clock() - begin) / CLOCKS_PER_SEC << endl;
 
 
-	cout << "Meplau!" << endl;
+	begin = clock();
+	for (int i = 0; i < SIZE; i++) {
+		tree.search(rand());
+	}
+	cout << "Running time in seconds: \t" << double(clock() - begin) / CLOCKS_PER_SEC << endl;
 
+	cout << endl;
+	getchar();
+	return 0;
+
+	begin = clock();
+	for (int i = 0; i < SIZE; i++) {
+		Q.push(rand());
+	}
+	cout << "Running time in seconds: \t" << double(clock() - begin) / CLOCKS_PER_SEC << endl;
+
+	
+	begin = clock();
+	for (int i = 0; i < SIZE; i++) {
+		int z = rand();
+		for (vector<int>::iterator p = Q.begin(); p != Q.end(); p++)
+		{
+			if (*p == z) {
+				break;
+			}
+		}
+	}
+	cout << "Running time in seconds: \t" << double(clock() - begin) / CLOCKS_PER_SEC << endl;
 	cout << endl << endl;
-	tmp.print();
+	
+
+
 	
 	getchar();
 	return 0;
